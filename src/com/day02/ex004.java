b1211b25cb2c3c33589dd8c38f2d0def11ce0526
Eugene Elantsev <eugene_el@mail.ru>
2022-06-26 15:49:48 +0700

    ex004 done

--- src/com/day02/ex004.java
+++ src/com/day02/ex004.java
@@ -8,10 +8,9 @@ package com.day02;
 
 import java.util.Scanner;
 
-
 public class ex004 {
 
-    static int iterations = 0;
+    static int iteration = 0;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -21,23 +20,37 @@ public class ex004 {
         char b = 'B';
         char c = 'C';
 
-        tower(n, a, b, c);
+        hanoiTower(n, a, b, c);
     }
 
-    public static void tower(int n, char a, char b, char c) {
+    public static void hanoiTower(int n, char a, char b, char c) {
+        // Если остался один диск, перекладываем с А на С
         if (n == 1) {
-            move(1, a, c);
-        } else {
+            showMove(1, a, c);
+        }
+        else {
             // Рекурсивно располагаем слой n-1 со стойки A, через C, до B
-            tower(n - 1, a, c, b);
-            move(n, a, c);
+            hanoiTower(n - 1, a, c, b);
+            showMove(n, a, c);
             // Рекурсивно располагаем слой n-1 со стойки B, через A, до C
-            tower(n - 1, b, a, c);
+            hanoiTower(n - 1, b, a, c);
         }
     }
 
-    public static void move(int n, char i, char j) {
-        iterations++;
-        System.out.println("Iteration " + iterations + " Disk " + n + " Move (from -> to) " + i + " -> " + j);
+    public static void showMove(int n, char i, char j) {
+        iteration++;
+        System.out.println("Iteration " + iteration + " Disk " + n + " Move (from -> to) " + i + " -> " + j);
     }
 }
+
+/*
+    Output:
+    How many discs? 3
+    Iteration 1 Disk 1 Move (from -> to) A -> C
+    Iteration 2 Disk 2 Move (from -> to) A -> B
+    Iteration 3 Disk 1 Move (from -> to) C -> B
+    Iteration 4 Disk 3 Move (from -> to) A -> C
+    Iteration 5 Disk 1 Move (from -> to) B -> A
+    Iteration 6 Disk 2 Move (from -> to) B -> C
+    Iteration 7 Disk 1 Move (from -> to) A -> C
+ */
