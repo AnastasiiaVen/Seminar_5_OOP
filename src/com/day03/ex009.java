ac3d9d778501374df77c7fd859074b6a352b2b2f
Eugene Elantsev <eugene_el@mail.ru>
2022-07-04 02:01:32 +0700

    move heapSort string

--- src/com/day03/ex009.java
+++ src/com/day03/ex009.java
@@ -11,8 +11,10 @@ public class ex009 {
         int[] initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1};
         System.out.println("Initial array:");
         System.out.println(Arrays.toString(initArray));
-        System.out.println("Sorted array:");
+
         heapSort(initArray, initArray.length);
+
+        System.out.println("Sorted array:");
         System.out.println(Arrays.toString(initArray));
     }
 
