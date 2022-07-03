e3edf62f780049fd9ace3a91d9932e8f5b431052
Eugene Elantsev <eugene_el@mail.ru>
2022-07-04 01:58:54 +0700

    ex009 done

--- src/com/day03/ex009.java
+++ src/com/day03/ex009.java
@@ -8,29 +8,53 @@ import java.util.Arrays;
 
 public class ex009 {
     public static void main(String[] args) {
-        int[] initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64};
+        int[] initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1};
+        System.out.println("Initial array:");
         System.out.println(Arrays.toString(initArray));
-        System.out.println(Arrays.toString(buildPyramid(initArray)));
+        System.out.println("Sorted array:");
+        heapSort(initArray, initArray.length);
+        System.out.println(Arrays.toString(initArray));
     }
 
-    public static int[] buildPyramid(int[] pyramid) {
-        int maxIndex = 0;
-        for (int i = pyramid.length / 2 - 1; i >= 0; i--) {
-            if (i * 2 + 2 <= pyramid.length - 1) {
-                if (pyramid[i * 2 + 2] > pyramid[i * 2 + 1]) maxIndex = i * 2 + 2;
+    public static void buildTree(int[] tree, int sortLength) {
+        // Для всех вершин дерева начиная с середины сортируемой части массива
+        // (движемся влево до нулевого индекса)
+        for (int i = sortLength / 2 - 1; i >= 0; i--) {
+            int maxIndex;
+            // Если у вершины два листа, выбираем больший
+            if (i * 2 + 2 <= sortLength - 1) {
+                if (tree[i * 2 + 2] > tree[i * 2 + 1]) maxIndex = i * 2 + 2;
                 else maxIndex = i * 2 + 1;
+            // Иначе один лист
             } else maxIndex = i * 2 + 1;
-            if (pyramid[i] < pyramid[maxIndex]) {
-                int temp = pyramid[i];
-                pyramid[i] = pyramid[maxIndex];
-                pyramid[maxIndex] = temp;
+            // Сравниваем лист с максимальным значением с вершиной, при большем значении
+            // дочернего элемента меняем его местами с вершиной
+            if (tree[i] < tree[maxIndex]) {
+                int temp = tree[i];
+                tree[i] = tree[maxIndex];
+                tree[maxIndex] = temp;
             }
-            System.out.println(Arrays.toString(pyramid));
         }
-        return pyramid;
     }
 
-    public static int[] heapSort(int[] sortArray) {
-        return sortArray;
+    public static void heapSort(int[] sortArray, int sortLength) {
+        // Выход из рекурсии: длина сортируемой части равна 0
+        if (sortLength == 0) return;
+        // Выстраивание элементов части массива в виде сортирующего дерева
+        buildTree(sortArray, sortLength);
+        // Замена местами первого и последнего элемента в сортируемой части
+        int temp = sortArray[0];
+        sortArray[0] = sortArray[sortLength - 1];
+        sortArray[sortLength - 1] = temp;
+        // Рекурсивно построение дерева и замена элементов для n - 1 элементов массива
+        heapSort(sortArray, sortLength - 1);
     }
 }
+
+/*
+    Output:
+    Initial array:
+    [17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1]
+    Sorted array:
+    [0, 1, 1, 3, 4, 7, 10, 17, 17, 25, 32, 64]
+ */
