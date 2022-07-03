05aabe35d5f46946f0f02b728eeb8279750038e4
Eugene Elantsev <eugene_el@mail.ru>
2022-07-03 12:14:17 +0700

    initial

---
+++ src/com/day03/ex006.java
@@ -0,0 +1,11 @@
+package com.day03;
+
+/*
+    Реализовать алгоритм сортировки слиянием
+ */
+
+public class ex006 {
+    public static void main(String[] args) {
+        int[] initArray = new int[] {2,1,3,4,6,8,2, 1};
+    }
+}
---
+++ src/com/day03/ex007.java
@@ -0,0 +1,11 @@
+package com.day03;
+
+/*
+    На шахматной доске расставить 8 ферзей так, чтобы они не били друг друга.
+ */
+
+public class ex007 {
+    public static void main(String[] args) {
+
+    }
+}
