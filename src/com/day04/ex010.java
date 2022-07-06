71abad326406aebf861d3fd41b9ca68663b342a7
Eugene Elantsev <eugene_el@mail.ru>
2022-07-06 14:09:03 +0700

    add calculation

--- src/com/day04/ex010.java
+++ src/com/day04/ex010.java
@@ -11,61 +11,70 @@ import java.util.ArrayDeque;
 
 public class ex010 {
     public static void main(String[] args) {
-        String initString = "1+2*(14-12)^3";
+        String initString = "3/3+2*(14-12)^3";
         System.out.println("Infix notation:");
         System.out.println(initString);
         System.out.println("Postfix notation:");
-        System.out.println(getPostfix(initString));
+        String postfixString = getPostfix(initString);
+        System.out.println(postfixString);
+        System.out.println("Calculation result:");
+        System.out.println(getResult(postfixString));
     }
 
     public static @NotNull String getPostfix(String inputString) {
+        // Разбиваем строку. Результат: [3, /, 3, +, 2, *, (, 14, -, 12, ), ^, 3]
         String[] stringArray = inputString.split("(?<=[-+*/^()])|(?=[-+*/^()])");
+        // StringBuilder для формирования постфиксной записи
         StringBuilder resultSb = new StringBuilder();
-        ArrayDeque<String> operatorStack = new ArrayDeque<>();
+        // Стек для операторов
+        ArrayDeque<String> operatorsStack = new ArrayDeque<>();
         for (String i : stringArray) {
             switch (i) {
                 case "+", "-" -> {
                     // Смотрим стек, если сверху есть приоритетный или равный по приоритету оператор,
                     // выносим его в строку и заносим оператор в стек
                     // (операторы имеют низший приоритет, если в стеке не скобка, выносим в строку)
-                    if (operatorStack.peek() != null) {
-                        if (!operatorStack.peekFirst().equals("(")) {
-                            resultSb.append(operatorStack.pop()).append(" ");
+                    if (operatorsStack.peek() != null) {
+                        if (!operatorsStack.peekFirst().equals("(")) {
+                            resultSb.append(operatorsStack.pop()).append(" ");
                         }
                     }
-                    operatorStack.push(i);
+                    operatorsStack.push(i);
                 }
                 case "*", "/" -> {
                     // Смотрим стек, если сверху есть приоритетный или равный по приоритету оператор,
                     // выносим его в строку и заносим оператор в стек
-                    if (operatorStack.peek() != null) {
-                        String temp = operatorStack.peekFirst();
+                    if (operatorsStack.peek() != null) {
+                        String temp = operatorsStack.peekFirst();
                         if (temp.equals("^") || temp.equals("*") || temp.equals("/")) {
-                            resultSb.append(operatorStack.pop()).append(" ");
+                            resultSb.append(operatorsStack.pop()).append(" ");
                         }
                     }
-                    operatorStack.push(i);
+                    operatorsStack.push(i);
                 }
                 // "^" максимальный приоритет, заносим в стек
                 // Открывающую скобку заносим в стек
-                case "^", "(" -> operatorStack.push(i);
+                case "^", "(" -> operatorsStack.push(i);
                 case ")" -> {
-                    // Переносим все операторы до появления открывающей скобки. Скобку удаляем из стека
+                    // Переносим все операторы из стека в строку до появления открывающей скобки.
+                    // Открывающую скобку удаляем из стека
                     while (true) {
-                        assert operatorStack.peekFirst() != null;
-                        if (operatorStack.peekFirst().equals("(")) break;
-                        resultSb.append(operatorStack.pop()).append(" ");
+                        assert operatorsStack.peekFirst() != null;
+                        if (operatorsStack.peekFirst().equals("(")) break;
+                        resultSb.append(operatorsStack.pop()).append(" ");
                     }
-                    operatorStack.pop();
+                    operatorsStack.pop();
                 }
                 default -> {
+                    // По умолчанию ожидаем число. Если проверка возвращает истину,
+                    // записываем число в строку
                     if (isNumeric(i)) resultSb.append(i).append(" ");
                 }
             }
         }
         // Выталкиваем оставшиеся операторы из стека, добавляем в строку
-        while (operatorStack.peek() != null) resultSb.append(operatorStack.pop()).append(" ");
-        return resultSb.toString();
+        while (operatorsStack.peek() != null) resultSb.append(operatorsStack.pop()).append(" ");
+        return resultSb.toString().trim();
     }
 
     public static boolean isNumeric(String checkString) {
@@ -74,4 +83,42 @@ public class ex010 {
         }
         return true;
     }
+
+    public static Integer getResult(String postfixString) {
+        // Стек для операндов
+        ArrayDeque<Integer> operandsStack = new ArrayDeque<>();
+        String[] postfixArray = postfixString.split(" ");
+        for (String i : postfixArray) {
+            // Если число, размещаем в стек операндов
+            if(isNumeric(i)) operandsStack.push(Integer.parseInt(i));
+            else {
+                // Берем из стека два верхних операнда
+                int y = operandsStack.pop();
+                int x = operandsStack.pop();
+                int tempResult = 0;
+                // Производим действие
+                switch (i) {
+                    case "+" -> tempResult = x + y;
+                    case "-" -> tempResult = x - y;
+                    case "*" -> tempResult = x * y;
+                    case "/" -> tempResult = x / y;
+                    case "^" -> tempResult = (int) Math.pow(x, y);
+                }
+                // Результат помещаем в стек
+                operandsStack.push(tempResult);
+            }
+        }
+        // Оставшееся в стеке значение является результатом вычисления
+        return operandsStack.pop();
+    }
 }
+
+/*
+    Output:
+    Infix notation:
+    3/3+2*(14-12)^3
+    Postfix notation:
+    3 3 / 2 14 12 - 3 ^ * +
+    Calculation result:
+    17
+ */
