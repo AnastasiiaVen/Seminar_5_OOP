426ad6f7f2cf6f4a6b4e4ea2e968c747abef632a
Eugene Elantsev <eugene_el@mail.ru>
2022-07-06 15:36:07 +0700

    ex012 done

--- src/com/day04/ex012.java
+++ src/com/day04/ex012.java
@@ -10,5 +10,52 @@ package com.day04;
     Пример 6: {a+]}{(d*3)} - ложь
  */
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
 public class ex012 {
+    public static void main(String[] args) {
+        String[] initStrings = {
+                "a+(d*3)",
+                "[a+(1*3)",
+                "[6+(3*3)]",
+                "{a}[+]{(d*3)}",
+                "<{a}+{(d*3)}>",
+                "{a+]}{(d*3)}"
+        };
+        for (String i : initStrings) {
+            System.out.printf("Bracket placement is correct? %s - %s\n",
+                    i, bracketsIsCorrect(i));
+        }
+    }
+
+    public static boolean bracketsIsCorrect(@NotNull String expression) {
+        char[] expressionArray = expression.toCharArray();
+        ArrayDeque<Character> brackets = new ArrayDeque<>();
+        HashMap<Character, Character> bracketsMap = new HashMap<>();
+        bracketsMap.put(')', '(');
+        bracketsMap.put('>', '<');
+        bracketsMap.put(']', '[');
+        bracketsMap.put('}', '{');
+        for (char i : expressionArray) {
+            switch (i) {
+                case '(', '<', '[', '{' -> brackets.push(i);
+                case ')', '>', ']', '}' -> {
+                    if (brackets.peek() == null || bracketsMap.get(i) != brackets.pop()) return false;
+                }
+            }
+        }
+        return brackets.peek() == null;
+    }
 }
+
+/*
+    Output:
+    Bracket placement is correct? a+(d*3) - true
+    Bracket placement is correct? [a+(1*3) - false
+    Bracket placement is correct? [6+(3*3)] - true
+    Bracket placement is correct? {a}[+]{(d*3)} - true
+    Bracket placement is correct? <{a}+{(d*3)}> - true
+    Bracket placement is correct? {a+]}{(d*3)} - false
+ */
