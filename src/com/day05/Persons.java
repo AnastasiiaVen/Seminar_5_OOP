2f912fb467af1f6950d71497b7a5c4405024d09d
Eugene Elantsev <eugene_el@mail.ru>
2022-07-12 17:55:10 +0700

    initial commit

---
+++ src/com/day05/Persons.java
@@ -0,0 +1,11 @@
+package com.day05;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Persons {
+    public static void main(String[] args) {
+        HashMap<Integer, ArrayList<String>> persons = new HashMap<>();
+    }
+}
