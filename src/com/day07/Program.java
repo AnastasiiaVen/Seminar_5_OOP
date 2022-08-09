6a5cc3b0d86ea29390bdddaac24071e827cba6b0
Eugene Elantsev <eugene_el@mail.ru>
2022-08-09 13:51:32 +0700

    initial commit

---
+++ src/com/day07/Program.java
@@ -0,0 +1,12 @@
+package com.day07;
+/*
+Иная реализация генеалогического дерева.
+Добавляем абстрактные типы, интерфейсы.
+Пробуем java Stream API
+ */
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
